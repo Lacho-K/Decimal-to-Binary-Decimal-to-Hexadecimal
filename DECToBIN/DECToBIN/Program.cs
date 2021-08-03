feef6a33e1bd02b4592b2ed63a0576ef199c82c6
Lacho123 <44599728+Lacho123@users.noreply.github.com>
2021-08-03 17:13:31 +0300

    Add files via upload

---
+++ DECToBIN/DECToBIN/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DECToBIN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dec = int.Parse(Console.ReadLine());
+            decToBin(dec);
+            for (int i = 1; i <= dec; i++)
+            {
+                decToBin(i);
+                Console.WriteLine();
+            }
+        }
+        static void decToBin(int dec)
+        {
+            Stack<int> bin = new Stack<int>();
+            while (dec != 0)
+            {
+                if (dec % 2 == 0)
+                {
+                    bin.Push(0);
+                }
+                else
+                {
+                    bin.Push(1);
+                }
+                dec /= 2;
+            }
+            foreach (var item in bin)
+            {
+                Console.Write(item);
+            }
+        }
+    }
+}
---
+++ DECToHEX/DECToHEX/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+namespace DECToHEX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            Stack<int> hex = new Stack<int>();
+            while (num != 0)
+            {
+                hex.Push(num % 16);
+                num /= 16;
+            }
+            foreach (var n in hex)
+            {
+                if(n < 10)
+                {
+                    Console.Write(n);
+                }
+                else
+                {
+                    if (n == 10) { Console.Write('A'); }
+                    if (n == 11) { Console.Write('B'); }
+                    if (n == 12) { Console.Write('C'); }
+                    if (n == 13) { Console.Write('D'); }
+                    if (n == 14) { Console.Write('E'); }
+                    if (n == 15) { Console.Write('F'); }
+                }
+            }
+        }
+    }
+}
